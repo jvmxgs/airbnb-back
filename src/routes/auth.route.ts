c881df4e7468dc81ebfaddab215da94e7300ce24
Victor <jvmxgs@gmail.com>
2023-09-12 22:07:56 -0600

    changed route login

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -2,7 +2,7 @@ import { Router } from 'express'
 import { authenticateUser, validateAccount } from '../controllers/auth.controller'
 
 export default (router: Router): void => {
-  router.post('/login', authenticateUser)
+  router.post('/api/login', authenticateUser)
 
   router.get('/validate', validateAccount)
 }
